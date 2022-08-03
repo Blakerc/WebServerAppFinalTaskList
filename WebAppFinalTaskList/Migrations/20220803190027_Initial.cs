34e434fba1c5e834697cf2563f0ecefb8ead0cd2
Ruairi Blake <76410753+Blakerc@users.noreply.github.com>
2022-08-03 15:12:48 -0400

    Creates db Table migration, populates with dummy data

---
+++ WebAppFinalTaskList/Migrations/20220803190027_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppFinalTaskList.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    TaskDateTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "Id", "Description", "TaskDateTime", "Title", "UserId" },
+                values: new object[] { 1, "This is a test task", new DateTime(2015, 12, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "Demo Task", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
