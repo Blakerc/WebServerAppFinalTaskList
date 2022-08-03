f8970a3e4340bbe6dfd12d4813fb816759bc0846
Ruairi Blake <76410753+Blakerc@users.noreply.github.com>
2022-08-03 14:36:11 -0400

    Task DbContext

---
+++ WebAppFinalTaskList/Models/DataLayer/TaskContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAppFinalTaskList.Models.DataLayer
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options)
+        : base(options)
+        { }
+        public DbSet<TaskModel> Tasks { get; set; }
+    }
+}
